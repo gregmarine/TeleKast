f740258c8f6bef9eba1d07b166c2d23822d6494c
gregmarine <gregmarine@4084da7f-2d56-455a-8786-96685c736e84>
2011-08-18 01:47:41 +0000

    Reverting trunk back to revision 15

--- trunk/defaults/preferences/prefs.js
+++ trunk/defaults/preferences/prefs.js
@@ -24,10 +24,3 @@ pref("network.protocol-handler.warn-external.http", false);
 pref("network.protocol-handler.warn-external.https", false);
 pref("network.protocol-handler.warn-external.ftp", false);
 pref("network.protocol-handler.expose-all", false);
-
-// Debugging preferences, for use during development:
-pref("browser.dom.window.dump.enabled", true);
-pref("javascript.options.showInConsole", true);
-pref("javascript.options.strict", true);
-pref("nglayout.debug.disable_xul_cache", true);
-pref("nglayout.debug.disable_xul_fastload", true);
